6178ddc6807e3defe29f234cb8c89979fb50dab9
Marcio Cunha <marcioabcd11@gmail.com>
2025-12-28 16:47:16 -0300

    FEAT: Cadastra cliente e verifica campos faltando e cliente repetido

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import cors from 'cors';
 import express from 'express';
+import cadastro from './controller/cadastroController.js'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
+app.post('/cadastro', cadastro)
 
 export default app;
